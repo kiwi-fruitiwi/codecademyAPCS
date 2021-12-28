1f2d2b5fdae60e2646c9a5949a0d79e3b2f4138b
kiwi-fruitiwi <zane.tian@gmail.com>
2021-12-27 16:10:19 -0800

    test simple project from codecademy

---
+++ src/Tree.java
@@ -0,0 +1,15 @@
+// Define your tree class in this file!
+public class Tree {
+    public static void main(String[] args) {
+        // introduce ourselves and print a tree to the screen
+        System.out.println("Hey there, I'm 🥝 and I'm learning to code in Java!");
+
+        System.out.println("I'm going to plant a tree today! Ready to get my hands dirty!'");
+
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println("*****");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+    }
+}
